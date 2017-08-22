b530936aa5da5f414f5d0e391a4ecaf4fe341f9d
sdpeyton <stephendpeyton@gmail.com>
2017-08-22 13:30:27 -0400

    adding bounding box geometry check

--- packages/gio-convert-geometry/index.js
+++ packages/gio-convert-geometry/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let _ = require('underscore');
+
 let convert_point = (geometry) => {
 	let point;
 	if (Array.isArray(geometry) && geometry.length === 2) { // array
@@ -16,16 +18,61 @@ let convert_point = (geometry) => {
 	}
 
 	if (!point) {
-		throw 'Invalid point object was used. Please use either a [lng, lat] array or GeoJSON point.';
+		throw `Invalid point object was used.
+			Please use either a [lng, lat] array or GeoJSON point.`;
 	}
 
 	return point;
+}
+
+let is_bbox = (coors) => {
+	if (coors.length === 5 && _.isEqual(coors[0], coors[4])) {
+		let lngs = coors.map(coor => coor[0]);
+		let lats = coors.map(coor => coor[1]);
+		if (lngs[0] === lngs[3] && lats[0] === lats[1] && lngs[1] === lngs[2]) {
+			return true;
+		}
+	}
+	return false;
+} 
+
+let convert_bbox = (geometry) => {
+	let bbox;
+	if (Array.isArray(geometry) && geometry.length === 4) { // array
+		bbox = geometry;
+	} else if (typeof geometry === 'string') { // stringified geojson
+		let geojson = JSON.parse(geometry);
+		console.error('check string');
+		let coors = geometry.coordinates[0];
+		if (geojson.type === 'Polygon' && is_bbox(coors)) {
+			console.error('checked')
+			let lngs = coors.map(coor => coor[0]);
+			let lats = coors.map(coor => coor[1]);
+			bbox = [Math.min(...lngs), Math.min(...lats), Math.max(...lngs), Math.max(...lats)];
+		}
+	} else if (typeof geometry === 'object') { // geojson
+		let coors = geometry.coordinates[0];
+		if (geometry.type === 'Polygon' && is_bbox(coors)) {
+			let lngs = coors.map(coor => coor[0]);
+			let lats = coors.map(coor => coor[1]);
+			bbox = [Math.min(...lngs), Math.min(...lats), Math.max(...lngs), Math.max(...lats)];
+		}
+	}
+
+	if (!bbox) {
+		throw `Invalid bounding box object was used. 
+			Please use either a [xmin, ymin, xmax, ymax] array or GeoJSON polygon.`
+	}
+
+	return bbox;
 }
 
 module.exports = (type_of_geometry, geometry) => {
 	try {
 		if (type_of_geometry === 'point') {
 			return convert_point(geometry);
+		} else if (type_of_geometry === 'bbox') {
+			return convert_bbox(geometry);
 		} else {
 			throw 'Invalid geometry type was specified. Please use either "point" or "polygon"';
 		}
