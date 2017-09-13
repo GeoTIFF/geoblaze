46ad1deeedc9731f2c8a2be19d4b5a3aaccccba7
sdpeyton <stephendpeyton@gmail.com>
2017-09-13 17:14:51 -0400

    updated tests for new bounding box features

--- test.js
+++ test.js
@@ -3,3 +3,6 @@
 require('./packages/gio-load/test');
 require('./packages/gio-convert-geometry/test');
 require('./packages/gio-identify/test');
+require('./packages/gio-sum/test');
+require('./packages/gio-mean/test');
+require('./packages/gio-mode/test');
