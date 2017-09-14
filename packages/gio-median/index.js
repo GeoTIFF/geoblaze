c8688f1ab024b0e6549629d5b3c074e7763240a9
sdpeyton <stephendpeyton@gmail.com>
2017-09-14 08:54:38 -0400

    added median feature

---
+++ packages/gio-median/index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+let get = require('../gio-get/index');
+let utils = require('../gio-utils/index');
+
+let get_median = values => {
+
+    // sort values
+    values.sort();
+    let values_length = values.length;
+
+    // pull middle value from sorted array
+    if (values_length % 2 === 0) {
+        return values[values_length / 2];
+    } else {
+        let middle = values_length / 2;
+        return values[Math.floor(middle)] + values[Math.ceil(middle)] / 2;
+    }
+}
+
+module.exports = (image, geom) => {
+    
+    try {
+        
+        if (utils.is_bbox(geom)) {
+
+            // grab array of values;
+            let values = get(image, geom);
+            let no_data_value = utils.get_no_data_value(image);
+
+            // get median
+            if (values.length === 1) { // one band
+                let filtered_values = values[0]
+                    .filter(value => value !== no_data_value);
+                return get_median(filtered_values)
+            } else {
+                return values
+                    .map(band => band.filter(value => value !== no_data_value))
+                    .map(get_median);
+            }
+        } else {
+            throw 'Non-Bounding Box geometries are currently not supported.'
+        }   
+    } catch(e) {
+        console.error(e);
+    }
+}
