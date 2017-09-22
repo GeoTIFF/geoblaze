502c73f2e5962b5bedb30ef86552a2b668c1beff
sdpeyton <stephendpeyton@gmail.com>
2017-09-21 21:23:22 -0400

    deleted unnecessary files

--- packages/gio-mean/mean.js
+++ packages/gio-mean/mean.js
@@ -6,12 +6,13 @@ let utils = require('../gio-utils/utils');
 module.exports = (image, geom) => {
     
     try {
-        
-        if (utils.is_bbox(geom)) {
 
-            // grab array of values;
+        let no_data_value = utils.get_no_data_value(image);
+        
+        if (utils.is_bbox(geom)) { // if geometry is a bounding box
+
+            // grab array of values
             let values = get(image, geom);
-            let no_data_value = utils.get_no_data_value(image);
 
             // run simple reduce to get average
             if (values.length === 1) { // one band
@@ -27,6 +28,14 @@ module.exports = (image, geom) => {
                         / band.length;
                 });
             }
+        } else if (utils.is_polygon(geom)) { // if geometry is a polygon
+
+            // get values in a bounding box around the geometry
+            let bbox = utils.get_bounding_box(geom);
+
+
+
+
         } else {
             throw 'Non-Bounding Box geometries are currently not supported.'
         }   
