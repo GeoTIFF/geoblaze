2c2b450e0943037c42b37d10314ff04bd30e2fe3
sdpeyton <stephendpeyton@gmail.com>
2017-09-30 10:57:47 -0400

    added functionality for polygon mean calculation

--- packages/gio-mean/mean.js
+++ packages/gio-mean/mean.js
@@ -1,15 +1,17 @@
 'use strict';
 
+let _ = require('underscore');
+
 let get = require('../gio-get/get');
 let utils = require('../gio-utils/utils');
+let intersect_polygon = require('../gio-intersect-polygon/intersect-polygon');
 
 module.exports = (image, geom) => {
     
     try {
 
-        let no_data_value = utils.get_no_data_value(image);
-        
         if (utils.is_bbox(geom)) { // if geometry is a bounding box
+            let no_data_value = utils.get_no_data_value(image);
 
             // grab array of values
             let values = get(image, geom);
@@ -29,15 +31,21 @@ module.exports = (image, geom) => {
                 });
             }
         } else if (utils.is_polygon(geom)) { // if geometry is a polygon
+            let sum = 0;
+            let num_values = 0;
+            
+            // the third argument of intersect_polygon is a function which
+            // is run on every value, we use it to increment the sum so we
+            // can later divide it by the total value count to get the mean
+            intersect_polygon(image, geom, value => {
+                sum += value;
+                num_values += 1;
+            });
 
-            // get values in a bounding box around the geometry
-            let bbox = utils.get_bounding_box(geom);
-
-
-
-
+            if (num_values > 0) return sum / num_values;
+            else throw 'No Values were found in the given geometry';
         } else {
-            throw 'Non-Bounding Box geometries are currently not supported.'
+            throw 'Only Bounding Box and Polygon geometries are currently supported.'
         }   
     } catch(e) {
         console.error(e);
