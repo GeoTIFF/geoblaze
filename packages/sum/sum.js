78a1293b2ebf66e44fffae891d635fd8aaf27e44
DanielJDufour <daniel.j.dufour@gmail.com>
2017-12-09 03:47:17 +0000

    Can sum with Blank Geometry

--- packages/sum/sum.js
+++ packages/sum/sum.js
@@ -9,7 +9,18 @@ module.exports = (georaster, geom) => {
     
     try {
         
-        if (utils.is_bbox(geom)) {
+        if (geom === null || geom === undefined) {
+
+            let no_data_value = georaster.no_data_value;
+            return georaster.values.map(band => { // iterate over each band which include rows of pixels
+                return band.reduce((sum_of_band, row) => { // reduce all the rows into one sum
+                    return sum_of_band + row.reduce((sum_of_row, cell_value) => { // reduce each row to a sum of its pixel values
+                        return cell_value !== no_data_value ? sum_of_row + cell_value : sum_of_row;
+                    }, 0);
+                }, 0);
+            });
+
+        } else if (utils.is_bbox(geom)) {
             geom = convert_geometry('bbox', geom);
 
             let values = get(georaster, geom);
