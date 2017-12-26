21a3bf4428ee27907325189fbb035f4a08a62dfe
DanielJDufour <daniel.j.dufour@gmail.com>
2017-12-26 22:15:10 +0000

    use wget for downloadign tiles not aws sync bc don't want to require aws

--- data/ghsl/download_data_for_travis_ci.sh
+++ data/ghsl/download_data_for_travis_ci.sh
@@ -1,1 +1,9 @@
-aws s3 sync s3://geoblaze/GHSL_Tiles/ tiles;
+declare -a relevant_tiles=("-90_20" "-80_20" "-60_-30" "10_50" "20_50" "30_40" "60_40")
+
+for name in "${relevant_tiles[@]}"
+do
+    tilename="GHS_POP_GPW42015_GLOBE_R2015A_54009_1k_v1_0_4326_${name}.tif";
+    wget "https://s3.amazonaws.com/geoblaze/GHSL_Tiles/${tilename}" -O "tiles/${tilename}"
+done
+
+
