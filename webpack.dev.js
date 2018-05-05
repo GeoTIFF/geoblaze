397501ad62561051d5ef51ca5e502a2330da1559
sdpeyton <stephendpeyton@gmail.com>
2018-05-05 08:24:14 -0400

    reconfigure webpack

---
+++ webpack.dev.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const base = require('./webpack.base.js');
+
+module.exports = merge(base, {
+  devServer: {
+    publicPath: '/',
+    historyApiFallback: true
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    })
+  ]
+});
---
+++ webpack.prod.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const base = require('./webpack.base.js');
+
+module.exports = merge(base, {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'geoblaze.js',
+    library: 'geoblaze',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      sourceMap: true
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ],
+});
