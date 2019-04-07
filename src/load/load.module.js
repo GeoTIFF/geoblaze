5afb198e851c03c9da00ac0e7747bb3e035377b7
DanielJDufour <daniel.j.dufour@gmail.com>
2019-04-07 20:22:30 +0000

    added async back in

--- src/load/load.module.js
+++ src/load/load.module.js
@@ -9,46 +9,40 @@ const inBrowser = typeof window === 'object';
 const fetch = inBrowser ? window.fetch : nodeFetch;
 const URL = inBrowser ? window.URL : nodeUrl.parse;
 
-function toArrayBuffer (response) {
+async function toArrayBuffer (response) {
   try {
     if (response.arrayBuffer) {
       return response.arrayBuffer();
     } else if (response.buffer) {
-      return response.buffer().then(buffer => {
-        return buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength);
-      });
+      const buffer = await response.buffer();
+      return buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength);
     }
   } catch (error) {
     throw error;
   }
 }
 
-function fetchWithErrorHandling (url) {
+async function fetchWithErrorHandling (url) {
   try {
-    return fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(ERROR_BAD_URL);
-        }
-        return response;
-      })
-      .catch(error => {
-        throw new Error(ERROR_BAD_URL);
-      });
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(ERROR_BAD_URL);
+    }
+    return await response;
   } catch (error) {
     throw new Error(ERROR_BAD_URL);
   }
 }
 
-function parseGeorasterWithErrorHandling (arrayBuffer) {
+async function parseGeorasterWithErrorHandling (arrayBuffer) {
   try {
-    return parseGeoraster(arrayBuffer);
+    return await parseGeoraster(arrayBuffer);
   } catch (error) {
     throw new Error(ERROR_PARSING_GEOTIFF);
   }
 }
 
-function load (urlOrFile) {
+async function load (urlOrFile) {
 
   if (!inBrowser && typeof urlOrFile === 'object') {
     throw new Error(ERROR_LOAD_FILE_OUTSIDE_BROWSER);
@@ -57,9 +51,10 @@ function load (urlOrFile) {
   const url = typeof urlOrFile === 'object' ? URL.createObjectURL(urlOrFile) : urlOrFile;
 
   if (!cache[url]) {
-    cache[url] = fetchWithErrorHandling(url)
-      .then(toArrayBuffer)
-      .then(parseGeorasterWithErrorHandling);
+    const response = await fetchWithErrorHandling(url);
+    const arrayBuffer = await toArrayBuffer(response);
+    const georaster = await parseGeorasterWithErrorHandling(arrayBuffer);
+    cache[url] = georaster;
   }
   return cache[url];
 }
