45e1d031347e760309776a0e6d6269faddac0c63
Tim Welch <tim.j.welch@gmail.com>
2021-06-27 09:09:12 -0700

    Rework Georaster interface

--- src/geoblaze.d.ts
+++ src/geoblaze.d.ts
@@ -1,12 +1,30 @@
 import { Point, Polygon, Feature, BBox } from "geojson";
 
-/** Migrate to georaster library */
-export class GeoRaster {
-  constructor(
-    data: object | string | Buffer | ArrayBuffer | number[][],
-    metadata: any,
-    debug: any
-  );
+export interface Georaster {
+  /** array of raster bands, each of which is a 2D array of cell values */
+  values: number[][][];
+  /** raster height in units of projection */
+  height: number;
+  /** raster width in units of projection */
+  width: number;
+  /** raster height in pixels */
+  pixelHeight: number;
+  /** raster width in pixels */
+  pixelWidth: number;
+  /** Projection identifier */
+  projection: unknown;
+  /** left boundary, in units of projection*/
+  xmin: number;
+  /** right boundary, in units of projection */
+  xmax: number;
+  /** top boundary (image y-axis is inverse of cartesian), in units of projection */
+  ymin: number;
+  /** bottom boundary (image y-axis is inverse of cartesian), in units of projection */
+  ymax: number;
+  /** cell value representing "no data" in raster */
+  noDataValue: number;
+  /** number of bands in raster */
+  numberOfRasters: number;
 }
 
 export type GeoblazeBBox = {
@@ -31,62 +49,62 @@ export type InputPoint = number[] | Point | Feature<Point>;
 export type InputBBox = BBox | GeoblazeBBox;
 
 export function bandArithmetic(
-  raster: GeoRaster,
+  raster: Georaster,
   operation: string
-): Promise<GeoRaster>;
+): Promise<Georaster>;
 
 export function get(
-  raster: GeoRaster,
+  raster: Georaster,
   geom: InputBBox | null | undefined,
   flat: boolean
-): Promise<GeoRaster>;
+): Promise<Georaster>;
 
 // Done
 export function histogram(
-  raster: GeoRaster,
+  raster: Georaster,
   geom: string | InputPolygon | null | undefined,
   options: HistogramOptions
 ): Histogram[];
 
 export function identify(
-  raster: GeoRaster,
+  raster: Georaster,
   geom?: string | InputPoint | null
 ): number[];
 
-export function load(urlOrFile: object | string): Promise<GeoRaster>;
+export function load(urlOrFile: object | string): Promise<Georaster>;
 
 export function max(
-  raster: GeoRaster,
+  raster: Georaster,
   geom: string | InputPolygon | null
-): Promise<GeoRaster>;
+): Promise<Georaster>;
 
 export function mean(
-  raster: GeoRaster,
+  raster: Georaster,
   geom: string | InputPolygon | null
-): Promise<GeoRaster>;
+): Promise<Georaster>;
 
 export function median(
-  raster: GeoRaster,
+  raster: Georaster,
   geom: string | InputPolygon | null
-): Promise<GeoRaster>;
+): Promise<Georaster>;
 
 export function min(
-  raster: GeoRaster,
+  raster: Georaster,
   geom: string | InputPolygon | null
-): Promise<GeoRaster>;
+): Promise<Georaster>;
 
 export function mode(
-  raster: GeoRaster,
+  raster: Georaster,
   geom: string | InputPolygon | null
-): Promise<GeoRaster>;
+): Promise<Georaster>;
 
 export function rasterCalculator(
-  raster: GeoRaster,
+  raster: Georaster,
   operation: (() => unknown) | string
-): GeoRaster;
+): Georaster;
 
 export function sum(
-  raster: GeoRaster,
+  raster: Georaster,
   geom: string | InputPolygon | null,
   test?: (cellValue: unknown) => boolean,
   debug?: boolean
