5666191de2162cd4cd6b58d3909d5e3caf3baa9d
Tim Welch <tim.j.welch@gmail.com>
2021-06-26 20:48:28 -0700

    initial declaration file push

---
+++ src/index.d.ts
@@ -0,0 +1,92 @@
+import { Point, Polygon, Feature, BBox } from "geojson";
+
+export declare type GeoblazeBBox = {
+  xmin: number;
+  xmax: number;
+  ymin: number;
+  ymax: number;
+};
+
+export interface HistogramOptions {
+  scaleType: "nominal" | "ratio";
+  numClasses: number;
+  classType: "equal-interval" | "quantile";
+}
+
+export interface Histogram {
+  [binKey: string]: number[];
+}
+
+export class GeoRaster {
+  constructor(
+    data: object | string | Buffer | ArrayBuffer | number[][],
+    metadata: any,
+    debug: any
+  );
+}
+
+export type InputPolygon = number[][][] | Polygon | Feature<Polygon>;
+export type InputPoint = number[] | Point | Feature<Point>;
+export type InputBBox = BBox | GeoblazeBBox;
+
+export function bandArithmetic(
+  raster: GeoRaster,
+  arithmetic: any
+): Promise<GeoRaster>;
+
+export function get(
+  raster: GeoRaster,
+  geom: any,
+  flat: boolean
+): Promise<GeoRaster>;
+
+// Done
+export function histogram(
+  raster: GeoRaster,
+  geom: string | InputPolygon | null | undefined,
+  options: HistogramOptions
+): Histogram[];
+
+export function identify(
+  raster: GeoRaster,
+  geom?: string | InputPoint | null
+): number[];
+
+export function load(urlOrFile: object | string): Promise<GeoRaster>;
+
+export function max(
+  raster: GeoRaster,
+  geom: string | InputPolygon | null
+): Promise<GeoRaster>;
+
+export function mean(
+  raster: GeoRaster,
+  geom: string | InputPolygon | null
+): Promise<GeoRaster>;
+
+export function median(
+  raster: GeoRaster,
+  geom: string | InputPolygon | null
+): Promise<GeoRaster>;
+
+export function min(
+  raster: GeoRaster,
+  geom: string | InputPolygon | null
+): Promise<GeoRaster>;
+
+export function mode(
+  raster: GeoRaster,
+  geom: string | InputPolygon | null
+): Promise<GeoRaster>;
+
+export function rasterCalculator(
+  raster: GeoRaster,
+  operation: string
+): GeoRaster;
+
+export function sum(
+  raster: GeoRaster,
+  geom: string | InputPolygon | null,
+  test?: any,
+  debug?: boolean
+): Promise<Array<number>>;
