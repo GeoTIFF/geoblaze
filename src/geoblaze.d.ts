9dd5fb8119ba13937c1953d2c0d67a7c698b094d
Tim Welch <tim.j.welch@gmail.com>
2021-06-27 10:27:50 -0700

    add mins, maxs, ranges

--- src/geoblaze.d.ts
+++ src/geoblaze.d.ts
@@ -1,7 +1,7 @@
 import { Point, Polygon, Feature, BBox } from "geojson";
 
 export interface Georaster {
-  /** array of raster bands, each of which is a 2D array of cell values */
+  /** raster values.  first dimension is raster band, remainder is 2D array of cell values */
   values: number[][][];
   /** raster height in units of projection */
   height: number;
@@ -23,8 +23,14 @@ export interface Georaster {
   ymax: number;
   /** cell value representing "no data" in raster */
   noDataValue: number;
-  /** number of bands in raster */
+  /** number of raster bands */
   numberOfRasters: number;
+  /** Minimum cell value for each raster band.  Indexed by band number */
+  mins: number[]
+  /** Maximum cell value for each raster band.  Indexed by band number */
+  maxs: number[]
+  /** difference between max and min for each raster band.  Indexed by band number */
+  ranges: number[]
 }
 
 export type GeoblazeBBox = {
