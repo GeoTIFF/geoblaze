f82a53169f5121208283c65e5c113ad63bd53dc8
Tim Welch <tim.j.welch@gmail.com>
2021-06-27 17:17:45 -0700

    solve default export object

--- src/geoblaze.d.ts
+++ src/geoblaze.d.ts
@@ -1,26 +1,29 @@
 import { Point, Polygon, Feature, BBox } from "geojson";
-import { Georaster } from "georaster"
+import { Georaster } from "georaster";
 
-export type GeoblazeBBox = {
+// re-rexport so that downstream user doesn't need to track it down
+export { Georaster } from "georaster"
+
+type GeoblazeBBox = {
   xmin: number;
   xmax: number;
   ymin: number;
   ymax: number;
 };
 
-export interface HistogramOptions {
+interface HistogramOptions {
   scaleType: "nominal" | "ratio";
   numClasses: number;
   classType: "equal-interval" | "quantile";
 }
 
-export interface Histogram {
+interface Histogram {
   [binKey: string]: number[];
 }
 
-export type InputPolygon = number[][][] | Polygon | Feature<Polygon>;
-export type InputPoint = number[] | Point | Feature<Point>;
-export type InputBBox = BBox | GeoblazeBBox;
+type InputPolygon = number[][][] | Polygon | Feature<Polygon>;
+type InputPoint = number[] | Point | Feature<Point>;
+type InputBBox = BBox | GeoblazeBBox;
 
 export const bandArithmetic: (
   raster: Georaster,
@@ -82,3 +85,20 @@ export const sum: (
   test?: (cellValue: number) => boolean,
   debug?: boolean
 ) => Promise<Array<number>>;
+
+// Create typed object matching default export in index.js
+declare const defaultExports: {
+  bandArithmetic: typeof bandArithmetic;
+  get: typeof get;
+  histogram: typeof histogram;
+  identify: typeof identify;
+  load: typeof load;
+  max: typeof max;
+  mean: typeof mean;
+  median: typeof median;
+  min: typeof min;
+  mode: typeof mode;
+  rasterCalculator: typeof rasterCalculator;
+  sum: typeof sum;
+};
+export default defaultExports;
