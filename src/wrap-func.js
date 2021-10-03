e001b29f5edd62e815ed3209286fbc83070930e7
DanielJDufour <daniel.j.dufour@gmail.com>
2021-10-03 16:04:58 -0700

    added wrap-func

---
+++ src/wrap-func.js
@@ -0,0 +1,16 @@
+/**
+ * @prettier
+ */
+import parse from "./parse";
+
+// this function basically allows you to pass a URL into a geoblaze function
+// and let geoblaze parse the GeoRaster for you
+export default function wrap(func) {
+  return (georaster, ...rest) => {
+    if (typeof georaster === "string") {
+      return parse(georaster).then(georaster => func(georaster, ...rest));
+    } else {
+      return func(georaster, ...rest);
+    }
+  };
+}
