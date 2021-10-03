6103a1f56f9bb76ab2b5b399080d870bc79ec19d
DanielJDufour <daniel.j.dufour@gmail.com>
2021-10-03 16:14:17 -0700

    added deprecation warning to load

--- src/load/index.js
+++ src/load/index.js
@@ -1,6 +1,8 @@
 import load from './load.module';
 
 /**
+ * This function is deprecated and you should try to migrate to using parse.
+ * This function will fetch the whole file in order to parse it!
  * The load function takes a url to a geotiff or geotiff file as an input
  * and returns a promise. The promise resolves as a georaster, which
  * can be used as input in other geoblaze methods, such as identify, sum,
