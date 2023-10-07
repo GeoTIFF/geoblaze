2b41bad2d16f65626515764f89986cdc78b206bf
DanielJDufour <daniel.j.dufour@gmail.com>
2023-10-07 14:55:26 -0400

    unzip mapspam wheat geotiff on setup

--- data/geotiff-test-data/setup.sh
+++ data/geotiff-test-data/setup.sh
@@ -2,6 +2,7 @@
 # download from https://github.com/GeoTIFF/test-data/
 wget https://github.com/GeoTIFF/test-data/archive/refs/heads/main.zip -O geotiff-test-data.zip
 unzip -j -o geotiff-test-data.zip "test-data-*/files/*" -d .
-rm geotiff-test-data.zip
+unzip spam2005v3r2_harvested-area_wheat_total.tiff.zip
+rm geotiff-test-data.zip spam2005v3r2_harvested-area_wheat_total.tiff.zip
 
 gdalwarp -t_srs EPSG:4326 gadas.tif gadas-4326.tif
