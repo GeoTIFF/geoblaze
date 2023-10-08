5eca8693b9394450b2d460dc9747923b8aeb1406
DanielJDufour <daniel.j.dufour@gmail.com>
2023-10-08 12:43:14 -0400

    updated gadm download link

--- data/gadm/download_gadm.sh
+++ data/gadm/download_gadm.sh
@@ -1,5 +1,6 @@
 if [ ! -f gadm28_levels.shp.zip ]; then
-    wget http://biogeo.ucdavis.edu/data/gadm2.8/gadm28_levels.shp.zip
+    # originally from http://biogeo.ucdavis.edu/data/gadm2.8/gadm28_levels.shp.zip
+    wget https://geoblaze.s3.amazonaws.com/gadm28_levels.shp.zip
 fi
 
 if [ ! -f gadm28_adm0.shp ]; then
