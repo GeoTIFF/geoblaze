63b1e905d795fa1e0d3d5442c2492437e5a7d6a2
DanielJDufour <daniel.j.dufour@gmail.com>
2024-06-07 22:19:08 -0400

    try to fix ci testing issues

--- src/stats/stats.test.js
+++ src/stats/stats.test.js
@@ -279,6 +279,7 @@ test("virtual resampling, contained", async ({ eq }) => {
 });
 
 test("virtual resampling, intersecting 4 pixels", async ({ eq }) => {
+  await new Promise(resolve => setTimeout(resolve, 5*1000)); // avoid failed fetching
   const url = "http://localhost:3000/data/geotiff-test-data/nz_habitat_anticross_4326_1deg.tif";
   const geojson = await fetch("http://localhost:3000/data/virtual-resampling/virtual-resampling-intersect.geojson").then(res => res.json());
   const result = await stats(url, geojson, null, null, { include_meta: false, rescale: true, vrm: [10, 10] });
