841ab5e533049a8f3bf79cde6aa6d61035f3192b
DanielJDufour <daniel.j.dufour@gmail.com>
2024-08-04 16:29:01 -0400

    updated documentation for geoblaze.sum

--- src/sum/index.js
+++ src/sum/index.js
@@ -9,6 +9,9 @@ import stat from "../stat";
  * @param {GeoRaster|ArrayBuffer|Blob|Buffer|File|String} georaster - geospatial gridded raster data
  * @param {Object} geometry - geometry can be an [xmin, ymin, xmax, ymax] array for a bounding box, [[[x00,y00],...,[x0n,y0n],[x00,y00]]...] for a polygon, a GeoJSON polygon object, or a string representation of a GeoJSON polygon object.
  * @param {Function} test - a filter function that takes in a pixel value and returns true or false whether it should be included in the sum calculation
+ * @param {Object} extraOptions
+ * @param {[number, number] | "minimal"} extraOptions.vrm - virtual resampling multiplier for the horizontal (x) and vertical (y) dimensions. In other words, how many times we divide each pixel horizontally and vertically to increase resolution. If you don't want to pass in the exact number of times to divide the pixels, pass "minimal" and geoblaze will calculate the minimal amount of divisions to make sure at least one pixel is intersected.
+ * @param {Boolean} extraOptions.rescale - whether we should rescale results based on virtual pixel size. It is highly recommended you set rescale to true if you set vrm.
  * @returns {Number[]} array of sums for each band
  * @example
  * // naip.tif has 4-bands: red, green, blue and near-infrared (nir)
