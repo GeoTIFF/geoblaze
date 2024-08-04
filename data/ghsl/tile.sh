f3d0f2fad6ad14e24a75dfdb8549ad6493ac3093
DanielJDufour <daniel.j.dufour@gmail.com>
2024-08-04 08:25:20 -0400

    improved logging into tile.sh

--- data/ghsl/tile.sh
+++ data/ghsl/tile.sh
@@ -18,7 +18,7 @@ for latitude in `seq 90 -10 -80`;
         projwin="$ulx $uly $lrx $lry";
         echo "projwin: $projwin";
         dst_dataset="tiles/GHS_POP_GPW42015_GLOBE_R2015A_54009_1k_v1_0_4326_${ulx}_${uly}${src_extension}";
-        #echo $dst_dataset
+        echo "running: gdal_translate -projwin $projwin $src_dataset $dst_dataset"
         gdal_translate -projwin $projwin $src_dataset $dst_dataset;
       done
   done   
